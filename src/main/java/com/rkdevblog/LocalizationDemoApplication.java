73924d640dc3162751016b541531a99c39e8d160
rajithkonara <rajithkonara@gmail.com>
2020-10-11 12:08:57 +0530

    init project

---
+++ src/main/java/com/rkdevblog/LocalizationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rkdevblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkdevblog/LocalizationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkdevblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
